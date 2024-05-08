553d10c3553bd66b606ed55e7865ccf0c2d57453
Liz Johnson <liz.jade.johnson@gmail.com>
2024-05-08 13:17:47 -0400

    Basic template seems to be working for mongo and react connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { useState } from 'react';
+import Menu from './components/Menu'
 
 function App() {
+  const [currentItem, setCurrentItem] = useState(null);
   return (
-    <div>
-      <Menu />
+    <div className='App'>
+      <h1>Restaurant Menu</h1>
+      <Menu currentItem={currentItem} setCurrentItem={setCurrentItem}/>
     </div>
   );
 }
